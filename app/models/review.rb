9697edb0c623d2091589261bd64d682ddd4ec0fd
Gabriel Souza <gsouzaa4@gmail.com>
2021-08-25 17:30:55 +0100

    rails-yelp | added reviews - seems to work

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
+
+  validates :rating, :content, presence: true
+  validates :rating, inclusion: { in: 0..5 }
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: 0..5, message: 'must be between 0 and 5.', allow_blank: true }
 end
